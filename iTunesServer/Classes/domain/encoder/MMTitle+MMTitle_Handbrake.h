3383b955a100d92c32605251f31829e5e4852b22
Olivier Larivain <devnull@localhost>
2012-03-04 17:39:01 -0800

    - fixed copyright header

--- iTunesServer/Classes/domain/encoder/MMTitle+MMTitle_Handbrake.h
+++ iTunesServer/Classes/domain/encoder/MMTitle+MMTitle_Handbrake.h
@@ -3,7 +3,7 @@
 //  iTunesServer
 //
 //  Created by Larivain, Olivier on 1/3/12.
-//  Copyright (c) 2012 Edmunds. All rights reserved.
+//  Copyright (c) 2012 kra. All rights reserved.
 //
 
 #import <MediaManagement/MMTitle.h>
